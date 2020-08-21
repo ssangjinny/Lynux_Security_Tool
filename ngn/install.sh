2eef06ab83ccc31bd8ac037747d02228a09f54d3
ssangjinny <69946430+ssangjinny@users.noreply.github.com>
2020-08-21 16:00:47 +0900

    Update install.sh

--- ngn/install.sh
+++ ngn/install.sh
@@ -1,6 +1,7 @@
-sudo apt-get install epel-release
-sudo apt-get install clamav
-sudo apt-get install chkrootkit
-sudo apt install apache2
-sudo apt-get install lynis
+sudo apt-get install -y epel-release
+sudo apt-get install -y libxcb-xinerama0
+sudo apt-get install -y clamav
+sudo apt-get install -y chkrootkit
+sudo apt install -y apache2
+sudo apt-get install -y lynis
 echo "설치 완료"
